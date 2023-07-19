d524086f826a4c4578b2026fe0100c71bba80630
Andriy-Kolokolov <koloandre22@gmail.com>
2023-07-19 17:41:22 +0200

    reworked cocktail controller

--- database/migrations/2023_07_19_144841_create_cocktails_table.php
+++ database/migrations/2023_07_19_144841_create_cocktails_table.php
@@ -14,11 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::create('cocktails', function (Blueprint $table) {
-            $table->id();
-            $table->name();
-            $table->recipe();
-            $table->image();
-            $table->timestamps();
+            $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->string('recipe', 600);
+            $table->string('image', 400);
         });
     }
 
