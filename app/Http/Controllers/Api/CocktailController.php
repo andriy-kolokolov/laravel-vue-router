a7dfbf1be8e33ab058356e0df6d660bf22336027
Andriy-Kolokolov <koloandre22@gmail.com>
2023-07-19 17:09:19 +0200

    reworked cocktail controller

--- app/Http/Controllers/Api/CocktailController.php
+++ app/Http/Controllers/Api/CocktailController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cocktail;
 
 class CocktailController extends Controller
 {
     public function index() {
+        $coctails = Cocktail::paginate(10);
+        return response()->json($coctails);
+    }
+
+    public function show(Cocktail $cocktail) {
 
     }
 }
